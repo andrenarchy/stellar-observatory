315b1430c00c5b01e26e1e09b9019da5e7c3fae2
André Gaul <andre@gaul.io>
2019-05-28 21:02:35 +0200

    nodes: add test

---
+++ stellarobservatory/nodes_test.py
@@ -0,0 +1,8 @@
+from .nodes import get_nodes_from_stellarbeat
+
+def test_get_nodes_from_stellarbeat():
+    nodes = get_nodes_from_stellarbeat()
+    assert isinstance(nodes, list)
+    for node in nodes:
+        assert isinstance(node, dict)
+        assert 'publicKey' in node
