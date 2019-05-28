8434a2beb6d86101e25e43ecf57876b44e8c17f1
André Gaul <andre@gaul.io>
2019-05-28 21:25:56 +0200

    nodes: add helpers

--- stellarobservatory/nodes.py
+++ stellarobservatory/nodes.py
@@ -4,3 +4,11 @@ import requests
 def get_nodes_from_stellarbeat():
     """Fetch nodes from stellarport.io"""
     return requests.get("https://api.stellarbeat.io/v1/nodes").json()
+
+def get_nodes_by_public_key(nodes):
+    """Get nodes by public key as a dictionary"""
+    return {node['publicKey']: node for node in nodes}
+
+def convert_public_keys_to_names(nodes_by_public_key, public_keys):
+    """Convert a set/list of node public keys to a set of names"""
+    return set([nodes_by_public_key[public_key]['name'] for public_key in public_keys])
