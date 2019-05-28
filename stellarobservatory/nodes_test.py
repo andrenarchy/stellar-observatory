77fec5e86e823daff78a8c150d5775a51729b0b4
André Gaul <andre@gaul.io>
2019-05-28 21:04:32 +0200

    nodes: fix linting

--- stellarobservatory/nodes_test.py
+++ stellarobservatory/nodes_test.py
@@ -1,6 +1,8 @@
+"""Nodes tests"""
 from .nodes import get_nodes_from_stellarbeat
 
 def test_get_nodes_from_stellarbeat():
+    """Test stellarbeat response"""
     nodes = get_nodes_from_stellarbeat()
     assert isinstance(nodes, list)
     for node in nodes:
