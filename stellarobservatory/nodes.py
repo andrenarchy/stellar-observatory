5cc484334145d98c8b5b2f92c2cddbd09eef16ad
André Gaul <andre@gaul.io>
2019-05-28 23:14:33 +0200

    nodes: add get_node_dependencies()

--- stellarobservatory/nodes.py
+++ stellarobservatory/nodes.py
@@ -12,3 +12,19 @@ def get_nodes_by_public_key(nodes):
 def convert_public_keys_to_names(nodes_by_public_key, public_keys):
     """Convert a set/list of node public keys to a set of names"""
     return set([nodes_by_public_key[public_key]['name'] for public_key in public_keys])
+
+def get_node_dependencies(nodes_by_public_key, public_key, dependencies=None):
+    """Get public keys of all nodes a node depends on (also transitively)"""
+    if dependencies is None:
+        dependencies = set([public_key])
+
+    def traverse_quorum_set_definition(quorum_set_definition):
+        for validator in quorum_set_definition['validators']:
+            if validator not in dependencies:
+                dependencies.add(validator)
+                get_node_dependencies(nodes_by_public_key, validator, dependencies)
+        for inner_quorum_set_definition in quorum_set_definition['innerQuorumSets']:
+            traverse_quorum_set_definition(inner_quorum_set_definition)
+
+    traverse_quorum_set_definition(nodes_by_public_key[public_key]['quorumSet'])
+    return dependencies
--- stellarobservatory/nodes_test.py
+++ stellarobservatory/nodes_test.py
@@ -1,10 +1,41 @@
 """Nodes tests"""
-from .nodes import get_nodes_from_stellarbeat
+from .nodes import get_nodes_from_stellarbeat, get_nodes_by_public_key, \
+    get_node_dependencies
 
-def test_get_nodes_from_stellarbeat():
-    """Test stellarbeat response"""
+def test_stellarbeat_nodes():
+    """Test get_nodes_from_stellarbeat()"""
     nodes = get_nodes_from_stellarbeat()
     assert isinstance(nodes, list)
     for node in nodes:
         assert isinstance(node, dict)
         assert 'publicKey' in node
+
+def test_node_dependencies():
+    """Test get_node_dependencies()"""
+    nodes = [
+        {
+            'publicKey': 'A',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'B'], 'innerQuorumSets': []}
+        },
+        {
+            'publicKey': 'B',
+            'quorumSet': {
+                'threshold': 2,
+                'validators': ['B'],
+                'innerQuorumSets':[{'threshold': 1, 'validators': ['A', 'C'], 'innerQuorumSets': []}]
+            }
+        },
+        {
+            'publicKey': 'C',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'C'], 'innerQuorumSets': []}
+        },
+        {
+            'publicKey': 'D',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'B', 'C', 'D'], 'innerQuorumSets': []}
+        },
+    ]
+    nodes_by_public_key = get_nodes_by_public_key(nodes)
+    dependencies_A = get_node_dependencies(nodes_by_public_key, 'A')
+    assert frozenset(dependencies_A) == frozenset(['A', 'B', 'C'])
+    dependencies_D = get_node_dependencies(nodes_by_public_key, 'D')
+    assert frozenset(dependencies_D) == frozenset(['A', 'B', 'C', 'D'])
