b734c6880001be173c0ab876d62af358f96b2247
André Gaul <andre@gaul.io>
2019-09-07 18:38:59 +0200

    utils/scc: document ordering of returned sccs

--- stellarobservatory/utils/scc.py
+++ stellarobservatory/utils/scc.py
@@ -4,7 +4,13 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 
 def get_strongly_connected_components(graph):
-    """Get strongly connected components for a directed graph"""
+    """
+    Get strongly connected components for a directed graph
+
+    The returned list of components is ordered by dependency, i.e.,
+    such that the nodes in the first component have no dependencies on
+    other components.
+    """
     nodes = list(graph.keys())
     node_index_by_node = {node: index for index, node in enumerate(nodes)}
     row_indexes = []
