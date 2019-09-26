1e4b5d88a855d5d8c8dd201d2d49971607573f4b
André Gaul <andre@gaul.io>
2019-09-26 22:22:19 +0200

    utils/graph: add get_induced_subgraph()

--- stellarobservatory/utils/graph.py
+++ stellarobservatory/utils/graph.py
@@ -7,3 +7,7 @@ def compute_transpose_graph(graph):
         for target_node in target_nodes:
             transpose[target_node].add(node)
     return transpose
+
+def get_induced_subgraph(graph, nodes):
+    """Get the nodes-induced subgraph G[S] for a graph G and a subset of nodes S"""
+    return {node: graph[node].intersection(nodes) for node in nodes}
--- stellarobservatory/utils/graph_test.py
+++ stellarobservatory/utils/graph_test.py
@@ -1,5 +1,5 @@
 """Test graph utilities"""
-from .graph import compute_transpose_graph
+from .graph import compute_transpose_graph, get_induced_subgraph
 
 def test_compute_transpose_graph():
     """Test compute_transpose_graph() with a graph"""
@@ -14,3 +14,16 @@ def test_compute_transpose_graph():
         2: {1, 3},
         3: {1, 2}
     }
+
+def test_get_induced_subgraph():
+    """Test get_induced_subgraph() with a graph"""
+    graph = {
+        1: {2, 3},
+        2: {3},
+        3: {2}
+    }
+    assert get_induced_subgraph(graph, {1, 2, 3}) == graph
+    assert get_induced_subgraph(graph, {1}) == {1: set()}
+    assert get_induced_subgraph(graph, {1, 2}) == {1: {2}, 2: set()}
+    assert get_induced_subgraph(graph, {2, 3}) == {2: {3}, 3: {2}}
+    assert get_induced_subgraph(graph, set()) == dict()
