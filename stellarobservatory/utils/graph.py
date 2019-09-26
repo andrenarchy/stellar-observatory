dcfedcb119c9e95421cfb6aeefb55dceb9858ac2
André Gaul <andre@gaul.io>
2019-09-26 22:28:44 +0200

    utils/graph: add get_indegrees()

--- stellarobservatory/utils/graph.py
+++ stellarobservatory/utils/graph.py
@@ -1,12 +1,17 @@
 """Utilities for graphs"""
 
-def compute_transpose_graph(graph):
-    """Compute the transpose graph"""
+def get_transpose_graph(graph):
+    """Get the transpose graph"""
     transpose = {node: set() for node in graph.keys()}
     for node, target_nodes in graph.items():
         for target_node in target_nodes:
             transpose[target_node].add(node)
     return transpose
+
+def get_indegrees(graph):
+    """Get a dict with indegrees for all nodes"""
+    transpose = get_transpose_graph(graph)
+    return {node: len(target_nodes) for node, target_nodes in transpose.items()}
 
 def get_induced_subgraph(graph, nodes):
     """Get the nodes-induced subgraph G[S] for a graph G and a subset of nodes S"""
--- stellarobservatory/utils/graph_test.py
+++ stellarobservatory/utils/graph_test.py
@@ -1,27 +1,27 @@
 """Test graph utilities"""
-from .graph import compute_transpose_graph, get_induced_subgraph
+from .graph import get_transpose_graph, get_indegrees, get_induced_subgraph
 
-def test_compute_transpose_graph():
-    """Test compute_transpose_graph() with a graph"""
-    graph = {
+graph = {
         1: {2, 3},
         2: {3},
         3: {2}
     }
-    transpose = compute_transpose_graph(graph)
+
+def test_get_transpose_graph():
+    """Test get_transpose_graph() with a graph"""
+    transpose = get_transpose_graph(graph)
     assert transpose == {
         1: set(),
         2: {1, 3},
         3: {1, 2}
     }
 
+def test_get_indegrees():
+    """Test get_indegrees() with a graph"""
+    assert get_indegrees(graph) == {1: 0, 2: 2, 3: 2}
+
 def test_get_induced_subgraph():
     """Test get_induced_subgraph() with a graph"""
-    graph = {
-        1: {2, 3},
-        2: {3},
-        3: {2}
-    }
     assert get_induced_subgraph(graph, {1, 2, 3}) == graph
     assert get_induced_subgraph(graph, {1}) == {1: set()}
     assert get_induced_subgraph(graph, {1, 2}) == {1: {2}, 2: set()}
