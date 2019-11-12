f169e21b8f6a1888712eec8f1f218814e63db16a
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-12 17:11:35 +0100

    update some comments

--- stellarobservatory/quorums.py
+++ stellarobservatory/quorums.py
@@ -12,13 +12,13 @@ FBAS = (IsSliceContained, AbstractSet[Type])
 
 
 def enumerate_quorums(fbas: FBAS):
-    """Enumerate all quorums of FBAS F (given by the FBAS function(set<T>, T) -> bool)."""
+    """Enumerate all quorums of FBAS F (given by the pair (function(set<T>, T) -> bool, set))."""
     (is_slice_contained, all_nodes) = fbas
     return traverse_quorums(is_slice_contained, set(), all_nodes)
 
 
 def traverse_quorums(is_slice_contained: IsSliceContained, committed: set, remaining: set):
-    """Given a FBAS F (by fbas) with set of nodes V
+    """Given a FBAS F (by is_slice_contained) with set of nodes V
     and given the sets: committed ⊆ V; R ⊆ V\\committed,
     enumerate all quorums Q of F with committed ⊆ Q ⊆ committed ∪ remaining"""
     perimeter = committed.union(remaining)
