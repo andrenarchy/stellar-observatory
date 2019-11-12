41d14e4a177e980e8f83d0e16a407d2aeb3610fd
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-12 12:32:53 +0100

    Optimize by defining an FBAS as a function  (set<T>, T) -> bool This returns if the FBAS has a slice for the given node T in the given set

--- stellarobservatory/quorums.py
+++ stellarobservatory/quorums.py
@@ -1,34 +1,53 @@
 """Torstens's quorum enumeration"""
-from .quorum_lachowski import greatest_quorum, next_split_node
-from .quorum_slices import get_dependencies_by_node
 
 
-def enumerate_quorums(slices_by_node):
-    """Enumerate all quorums of FBAS F (given by slices_by_node)."""
-    deps_by_node = get_dependencies_by_node(slices_by_node)
-    fbas_info = {
-        'deps_by_node': deps_by_node,
-        'slices_by_node': slices_by_node
-    }
-    all_nodes = set(slices_by_node.keys())  # all nodes need to be present as keys here
-    return traverse_quorums(fbas_info, set(), all_nodes)
+from typing import Callable
+from typing import AbstractSet
+from typing import Type
+
+# Allow for defining an FBAS as a function: (set<T>, T) -> bool.
+# This function returns True, iff the FBAS has a slice for the given node T in the given
+# set.
+FBAS = Callable[[AbstractSet[Type], Type], bool]
+
+def enumerate_quorums(fbas: FBAS, all_nodes):
+    """Enumerate all quorums of FBAS F (given by the FBAS function(set<T>, T) -> bool)."""
+    return traverse_quorums(fbas, set(), all_nodes)
 
 
-def traverse_quorums(fbas_info, committed, remaining):
+def traverse_quorums(fbas, committed, remaining):
     """Given a FBAS F (by fbas_info) with set of nodes V
     and given the sets: committed ⊆ V; R ⊆ V\\committed,
     enumerate all quorums Q of F with committed ⊆ Q ⊆ committed ∪ remaining"""
     perimeter = committed.union(remaining)
-    greatest_q = greatest_quorum(perimeter, fbas_info['slices_by_node'])
+    greatest_q = greatest_quorum(fbas, perimeter)
     if greatest_q == set() or not committed.issubset(greatest_q):
         return
     yield frozenset(greatest_q)
 
     current = greatest_q.difference(committed)
     while not current == set():
-        # v ← pick from W:
-        node = next_split_node(current, fbas_info['deps_by_node'])
-        yield from traverse_quorums(fbas_info,
+        # v ← pick from W = current
+        # (note pylint complains:
+        # Do not raise StopIteration in generator, use return statement instead
+        # but this can't happen as current != set())
+        # pylint: disable=R1708
+        node = next(iter(current))
+        yield from traverse_quorums(fbas,
                                     greatest_q.difference(current),
                                     current.difference({node}))
         current = current.difference({node})
+
+
+def greatest_quorum(fbas, nodes):
+    """
+    Return greatest quorum contained in nodes or empty set (if there is no such quorum).
+    """
+    while True:
+        next_u = set()
+        for node in nodes:
+            if fbas(nodes, node):
+                next_u.add(node)
+        if len(nodes) == len(next_u):
+            return next_u
+        nodes = next_u
--- stellarobservatory/quorums_test.py
+++ stellarobservatory/quorums_test.py
@@ -1,8 +1,9 @@
 """Test for Torstens's quorum enumeration"""
 from .quorums import enumerate_quorums
+from .quorum_lachowski import contains_slice
 
 def test_enumerate_quorums():
-    """Test enumerate_quorums()"""
+    """Test enumerate_quorums() with simple example"""
     slices_by_node = {
         1: [{1, 2, 3, 7}],
         2: [{1, 2, 3, 7}],
@@ -12,10 +13,49 @@ def test_enumerate_quorums():
         6: [{4, 5, 6, 7}],
         7: [{7}],
     }
-    quorums = list(enumerate_quorums(slices_by_node))
+
+    def ex28_fbas(nodes_subset, node) -> bool:
+        return contains_slice(nodes_subset, slices_by_node, node)
+
+    quorums = list(enumerate_quorums(ex28_fbas, {1, 2, 3, 4, 5, 6, 7}))
     assert set(quorums) == set(
         [frozenset({7}),
          frozenset({4, 5, 6, 7}),
          frozenset({1, 2, 3, 7}),
          frozenset({1, 2, 3, 4, 5, 6, 7})]
     )
+
+def test_enumerate_quorums_stellar_core():
+    """Test enumerate_quorums() with stellar core style fbas"""
+    # init test:
+    stellar_core_orgs = [
+        {'name': "B", 'nodes': ["1", "2", "3"], 'limit': 2},
+        {'name': "A", 'nodes': ["1", "2", "3"], 'limit': 2},
+        {'name': "C", 'nodes': ["1", "2", "3"], 'limit': 2},
+        {'name': "D", 'nodes': ["1", "2", "3"], 'limit': 2},
+        {'name': "E", 'nodes': ["1", "2", "3"], 'limit': 2},
+        {'name': "F", 'nodes': ["1", "2", "3", "4", "5"], 'limit': 3}
+    ]
+    stellar_core_nodes = set()
+    for org in stellar_core_orgs:
+        name, nodes = org['name'], org['nodes']
+        for node in nodes:
+            stellar_core_nodes.add(name + node)
+
+    def stellar_core(subset: set, _: str) -> bool:
+        sufficient_orgs = 0
+        for org in stellar_core_orgs:
+            name, nodes, limit = org['name'], org['nodes'], org['limit']
+            sufficient_nodes = 0
+            for org_node in nodes:
+                node = str(name + org_node)
+                if node in subset:
+                    sufficient_nodes += 1
+            if sufficient_nodes >= limit:
+                sufficient_orgs += 1
+        return sufficient_orgs >= 5
+
+
+    #logging.debug("all nodes len: %s", stellar_core_nodes)
+    quorums = list(enumerate_quorums(stellar_core, stellar_core_nodes))
+    assert len(quorums) == 114688
