ce85a00519fae3905d418eafb0dec5fef06d75bc
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-18 23:11:25 +0100

    Test fails for current paper version of lachowski (torsten's version)

---
+++ stellarobservatory/quorum_intersection.py
@@ -0,0 +1,65 @@
+import logging
+from typing import Callable, Set, Type, Tuple
+
+from stellarobservatory.quorums import greatest_quorum
+
+def quorum_intersection(fbas: Tuple[Callable[[Set[Type], Type], bool], Set[Type]]):
+    (is_slice_contained, all_nodes) = fbas
+    logging.info("Fbas: %s", all_nodes)
+    for quorum in traverse_min_quorums(is_slice_contained, set(), all_nodes):
+        logging.info("traversing: %s", all_nodes)
+        greatest_q = greatest_quorum(is_slice_contained, all_nodes.difference(quorum), set())
+        if greatest_q != set():
+            logging.info("Found two disjoint quorums: %, %", quorum, greatest_q)
+            return False
+    return True
+
+def contains_proper_sub_quorum(is_slice_contained: Callable[[Set[Type], Type], bool],
+                               subset_nodes: set):
+    """Takes an FBAS with set of nodes V; and a subset U of V and
+    returns whether there is a quorum Q not fully contained U"""
+    for node in subset_nodes:
+        if greatest_quorum(is_slice_contained, subset_nodes.difference({node}), set()) != set():
+            return True
+    return False
+
+
+def traverse_min_quorums(is_slice_contained: Callable[[Set[Type], Type], bool],
+                         committed: set,   # U
+                         remaining: set):  # R
+    """Enumerate all min quorums Q with U ⊆ Q ⊆ U∪R and |Q|≤|V|/2"""
+    logging.debug("Yay, traversing ...")
+    if len(committed) > len(remaining)/2:
+        logging.debug("Booo, returning %s, %s", committed, remaining)
+        return
+    # TODO figure out if set() is the best "lower bound" here for greatest_quorum
+    greatest_q = greatest_quorum(is_slice_contained, committed, set())
+    if greatest_q != set():
+        if committed == greatest_q and not contains_proper_sub_quorum(is_slice_contained,
+                                                                      committed):
+            logging.debug("Yielding: %s", committed)
+            yield committed
+    else:
+        perimeter = committed.union(remaining)
+        # TODO figure out if set() is the best "lower bound" here for greatest_quorum
+        logging.debug("found ourself elsing: %s ...", remaining != set() and not committed.issubset(greatest_quorum(is_slice_contained,
+                                                                         perimeter,
+                                                                         set())))
+        logging.debug("...%s, %s", committed, remaining)
+
+        if remaining != set() and not committed.issubset(greatest_quorum(is_slice_contained,
+                                                                         perimeter,
+                                                                         set())):
+            # v ← pick from R:
+            # (note pylint complains:
+            # Do not raise StopIteration in generator, use return statement instead
+            # but this can't happen as current != set())
+            # pylint: disable=R1708
+            node = next(iter(remaining))
+            remaining_without_v = remaining.difference({node})
+            yield from traverse_min_quorums(is_slice_contained,
+                                            committed,
+                                            remaining_without_v)
+            yield from traverse_min_quorums(is_slice_contained,
+                                            committed.union({node}),
+                                            remaining_without_v)
---
+++ stellarobservatory/quorum_intersection_test.py
@@ -0,0 +1,21 @@
+import logging
+
+from stellarobservatory.quorum_lachowski import contains_slice
+
+from stellarobservatory.quorum_intersection import quorum_intersection
+
+
+def test_has_quorum_intersection_false():
+    """Test failing has_quorum_intersection()"""
+    # Basically two disjoint quorums {A, B} and {D} (two SCCs)
+    slices_by_node = {
+        'A': [{'A', 'B'}, {'A', 'C'}, {'A', 'B', 'C'}],
+        'B': [{'A', 'B'}],
+        'C': [{'A', 'B', 'C', 'D'}],
+        'D': [{'D'}]
+    }
+
+    def is_slice_contained(nodes_subset, node) -> bool:
+        return contains_slice(nodes_subset, slices_by_node, node)
+    # TODO assert
+    quorum_intersection((is_slice_contained, {'A', 'B', 'C', 'D'}))
