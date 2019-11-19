fdff56828e2be219ce0de9b735f55fae86d786c3
André Gaul <andre@gaul.io>
2019-11-19 17:20:46 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stellar-observatory",
-    version="1.0a4",
+    version="1.0a5",
     author="André Gaul",
     author_email="andre@gaul.io",
     description="Python package for analyzing the Stellar network",
