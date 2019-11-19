9daf7944a3afabd1f148d8caf7526d44bf1d6526
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-19 09:07:14 +0100

    Debugging: Change to `if |U|>|V|/2 return` -> all tests fail

--- stellarobservatory/quorum_intersection.py
+++ stellarobservatory/quorum_intersection.py
@@ -9,7 +9,7 @@ def quorum_intersection(fbas: Tuple[Callable[[Set[Type], Type], bool], Set[Type]
     """Takes an FBAS with set of nodes V and returns True iff F has quorum intersection.
     It prints two disjoint quorums otherwise."""
     (is_slice_contained, all_nodes) = fbas
-    for quorum in traverse_min_quorums(is_slice_contained, set(), all_nodes):
+    for quorum in traverse_min_quorums(fbas, set(), all_nodes):
         greatest_q = greatest_quorum(is_slice_contained, all_nodes.difference(quorum), set())
         if greatest_q != set():
             logging.info("Found two disjoint quorums: %s, %s", quorum, greatest_q)
@@ -17,21 +17,25 @@ def quorum_intersection(fbas: Tuple[Callable[[Set[Type], Type], bool], Set[Type]
     return True
 
 
-def contains_proper_sub_quorum(is_slice_contained: Callable[[Set[Type], Type], bool],
+def contains_proper_sub_quorum(fbas: Tuple[Callable[[Set[Type], Type], bool], Set[Type]],
                                subset_nodes: set):
     """Takes an FBAS with set of nodes V; and a subset U of V and
     returns whether there is a quorum Q not fully contained U"""
+    (is_slice_contained, _) = fbas
     for node in subset_nodes:
         if greatest_quorum(is_slice_contained, subset_nodes.difference({node}), set()) != set():
             return True
     return False
 
 
-def traverse_min_quorums(is_slice_contained: Callable[[Set[Type], Type], bool],
+def traverse_min_quorums(fbas: Tuple[Callable[[Set[Type], Type], bool], Set[Type]],
                          committed: set,  # U
                          remaining: set):  # R
     """Enumerate all min quorums Q with U ⊆ Q ⊆ U∪R and |Q|≤|V|/2"""
-    if len(committed) > len(remaining) / 2:
+    # TODO shouldn't this be len(remaining) / 2 + 1? The paper says U|>|V|/2
+    (is_slice_contained, all_nodes) = fbas
+    if len(committed) > len(all_nodes) / 2:
+        logging.debug("len(committed) > len(all_nodes) / 2: %s; with committed: %s, remaining: %s, all: %s,  len(all_nodes) / 2 =%s", len(committed) > len(all_nodes) / 2, committed, remaining, all_nodes,  len(all_nodes) / 2)
         return
     # TODO figure out if set() is the best "lower bound" here for greatest_quorum
     greatest_q = greatest_quorum(is_slice_contained, committed, set())
@@ -52,9 +56,9 @@ def traverse_min_quorums(is_slice_contained: Callable[[Set[Type], Type], bool],
             # pylint: disable=R1708
             node = next(iter(remaining))
             remaining_without_v = remaining.difference({node})
-            yield from traverse_min_quorums(is_slice_contained,
+            yield from traverse_min_quorums(fbas,
                                             committed,
                                             remaining_without_v)
-            yield from traverse_min_quorums(is_slice_contained,
+            yield from traverse_min_quorums(fbas,
                                             committed.union({node}),
                                             remaining_without_v)
