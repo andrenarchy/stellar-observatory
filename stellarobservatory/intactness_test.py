7b9948844f7b49f5904d66df79362ec71c41a819
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-20 13:06:57 +0100

    add simple test case

---
+++ stellarobservatory/intactness_test.py
@@ -0,0 +1,19 @@
+"""Tests for Torstens's intactNode Algorithm"""
+from stellarobservatory.intactness import intact_nodes
+from stellarobservatory.quorums import contains_slice
+
+
+def test_intact_nodes():
+    """Test intact_nodes (Example 4.13)"""
+    slices_by_node = {
+        'A': [{'A', 'B'}],
+        'B': [{'A', 'B', 'C', 'D'}],
+        'C': [{'B', 'C'}, {'C', 'D'}],
+        'D': [{'B', 'D'}, {'C', 'D'}]
+    }
+
+    def is_slice_contained(nodes_subset, node, without_d) -> bool:
+        return contains_slice(nodes_subset, slices_by_node, node, without_d)
+
+    res = intact_nodes((is_slice_contained, {'A', 'B', 'C', 'D'}), {'A'})
+    assert res == {'C', 'D'}
