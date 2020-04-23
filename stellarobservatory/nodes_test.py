96ec3ad74cae7d5cbe1b2bf92937d64618372cbe
Carlos Echeverría <carlos@Dolores.local>
2020-04-23 14:11:47 +0200

    Added test for new get_trust_graph() function

--- stellarobservatory/nodes_test.py
+++ stellarobservatory/nodes_test.py
@@ -1,6 +1,6 @@
 """Nodes tests"""
 from .nodes import get_nodes_from_stellarbeat, get_nodes_by_public_key, \
-    get_node_dependencies
+    get_node_dependencies, get_trust_graph
 
 def test_stellarbeat_nodes():
     """Test get_nodes_from_stellarbeat()"""
@@ -55,3 +55,49 @@ def test_node_dependencies():
     assert frozenset(dependencies_d) == frozenset(['A', 'B', 'C', 'D'])
     dependencies_e_int = get_node_dependencies(nodes_by_public_key, 'E', transitive=False)
     assert frozenset(dependencies_e_int) == frozenset(['A', 'D', 'E'])
+
+def test_get_trust_graph():
+    """Test get_trust_graph()"""
+    nodes = [
+        {
+            'publicKey': 'A',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'B'], 'innerQuorumSets': []}
+        },
+        {
+            'publicKey': 'B',
+            'quorumSet': {
+                'threshold': 2,
+                'validators': ['B'],
+                'innerQuorumSets':[
+                    {'threshold': 1, 'validators': ['A', 'C'], 'innerQuorumSets': []}
+                ]
+            }
+        },
+        {
+            'publicKey': 'C',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'C'], 'innerQuorumSets': []}
+        },
+        {
+            'publicKey': 'D',
+            'quorumSet': {'threshold': 2, 'validators': ['A', 'B', 'C', 'D'], 'innerQuorumSets': []}
+        },
+        {
+            'publicKey': 'E',
+            'quorumSet':{
+                'threshold': 2,
+                'validators': ['A'],
+                'innerQuorumSets':[
+                    {'threshold': 1, 'validators': ['A', 'D'], 'innerQuorumSets': []}
+                ]
+            }
+        },
+    ]
+    nodes_by_public_key = get_nodes_by_public_key(nodes)
+    trust_graphs = get_trust_graph(nodes_by_public_key)
+    assert isinstance(trust_graphs, dict)
+    assert len(trust_graphs) == 5
+    assert trust_graphs['A'] == frozenset(['A', 'B'])
+    assert trust_graphs['B'] == frozenset(['A', 'B', 'C'])
+    assert trust_graphs['C'] == frozenset(['A', 'C'])
+    assert trust_graphs['D'] == frozenset(['A', 'B', 'C', 'D'])
+    assert trust_graphs['E'] == frozenset(['A', 'D'])
