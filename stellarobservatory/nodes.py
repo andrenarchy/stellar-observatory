277785fdd9193edaa1d917d5124e2d9407050743
Carlos Echeverría <carlos@Dolores.local>
2020-04-23 13:35:25 +0200

    Added get_trust_graph() function mapping nodes to dependencies

--- stellarobservatory/nodes.py
+++ stellarobservatory/nodes.py
@@ -35,3 +35,11 @@ def get_node_dependencies(nodes_by_public_key, public_key, dependencies=None, tr
 
     traverse_quorum_set_definition(nodes_by_public_key[public_key]['quorumSet'])
     return dependencies
+
+def get_trust_graph(nodes_by_public_key):
+    """Map each node's public key to its trust graph (the set of nodes it references in its quorum slices). The node itself is excluded."""
+    graph={}
+    for key in nodes_by_public_key:
+        nodes_intrans = get_node_dependencies(nodes_by_public_key, key, dependencies=set(),transitive=False)
+        graph[key] = set(nodes_intrans)
+    return graph
