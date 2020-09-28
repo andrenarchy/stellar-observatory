4efe0821dc12c042dc1422fba4be7ab62db1aae0
André Gaul <andre@gaul.io>
2020-09-28 19:01:25 +0200

    remove quorum_slices

--- stellarobservatory/__init__.py
+++ stellarobservatory/__init__.py
@@ -1,7 +1,7 @@
 """Stellar Observatory"""
 from . import intactness, nodes, quorum_intersection, quorum_slice_definition, \
-    quorum_slices, quorum, quorums, stellarbeat, utils
+    quorum, quorums, stellarbeat, utils
 
 __all__ = ['intactness', 'nodes', 'quorum_intersection',
-           'quorum_slice_definition', 'quorum_slices', 'quorum',
+           'quorum_slice_definition', 'quorum',
            'quorums', 'stellarbeat', 'utils']
