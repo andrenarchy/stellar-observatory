6433427a48a3d26b5f0779ec232c04a104274845
André Gaul <andre@gaul.io>
2020-10-01 21:47:30 +0200

    add type definitions to slice_definition

--- stellarobservatory/quorum_slice_definition.py
+++ stellarobservatory/quorum_slice_definition.py
@@ -1,8 +1,18 @@
 """Quorum slice definitions"""
 
 from itertools import chain, combinations, product
+from typing import TypedDict, Dict, Set, List, Any
 
-def get_direct_dependencies(definitions_by_node, node):
+Definition = TypedDict('Definition', {
+    'threshold': int,
+    'nodes': Set[str],
+    # NOTE: use List['Definition] when  https://github.com/python/mypy/issues/731 is fixed
+    'children_definitions': Any
+})
+
+Definitions = Dict[str, Definition]
+
+def get_direct_dependencies(definitions_by_node: Definitions, node: str):
     """Get direct dependencies of a node"""
     dependencies = set([node])
 
@@ -18,7 +28,7 @@ def get_direct_dependencies(definitions_by_node, node):
     dependencies.discard(node)
     return dependencies
 
-def get_transitive_dependencies(definitions_by_node, node):
+def get_transitive_dependencies(definitions_by_node: Definitions, node: str):
     """Get transitive dependencies of a node"""
     dependencies = set()
     pending_dependencies = set([node])
@@ -33,7 +43,7 @@ def get_transitive_dependencies(definitions_by_node, node):
     dependencies.discard(node)
     return dependencies
 
-def get_trust_graph(definitions_by_node):
+def get_trust_graph(definitions_by_node: Definitions):
     """
     Map each node's public key to its trust graph (the set of nodes it
     references in its quorum slice definitionss). The node itself is excluded.
@@ -41,7 +51,7 @@ def get_trust_graph(definitions_by_node):
     return {node: get_direct_dependencies(definitions_by_node, node) \
         for node in definitions_by_node.keys()}
 
-def remove_from_definition(definition, node):
+def remove_from_definition(definition: Definition, node: str):
     """Return quorum slice definition with the given node removed and the threshold reduced by 1"""
     threshold = definition['threshold']
     nodes = definition['nodes']
@@ -59,7 +69,7 @@ def remove_from_definition(definition, node):
         'children_definitions': children_definitions
     }
 
-def get_normalized_definition(definition, node):
+def get_normalized_definition(definition: Definition, node: str):
     """Returns the node's quorum slice definition as Stellar Core preprocesses it
 
     The node is required on root level and removed from rest of quorum slice definition"""
@@ -69,7 +79,7 @@ def get_normalized_definition(definition, node):
         'children_definitions': [remove_from_definition(definition, node)]
     }
 
-def generate_quorum_slices(definition, mode='economic'):
+def generate_quorum_slices(definition: Definition, mode='economic'):
     """Generate all quorum slices for a quorum slice definition
 
     'economic' mode only returns quorum slices of size equal to the threshold,
@@ -99,7 +109,7 @@ def generate_quorum_slices(definition, mode='economic'):
             for quorum_slice_product in quorum_slice_products]
 
 
-def satisfies_definition(candidate, definition):
+def satisfies_definition(candidate: Set[str], definition: Definition):
     '''Checks if the candidate set contains a slice for the provided definition'''
     satisfied = len(definition['nodes'].intersection(candidate))
     for children_definition in definition['children_definitions']:
